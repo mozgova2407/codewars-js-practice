17df161c61adf01bbedebfb3e7ea787fa944430f
mozgova2407 <150112891+mozgova2407@users.noreply.github.com>
2026-06-26 23:22:28 +0300

    Create first-task.js

---
+++ kata-8kyu/first-task.js
@@ -0,0 +1,8 @@
+// Task: Messi Goals (Codewars 8 kyu)
+
+function goals(laLiga, copaDelRey, champions) {
+  return laLiga + copaDelRey + champions;
+}
+
+// test
+console.log(goals(5, 10, 2)); // 17
